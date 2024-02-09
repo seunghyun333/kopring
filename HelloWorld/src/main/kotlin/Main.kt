bc13ac324b6c98ee998cd5328c39f4a725dfcb48
seunghyun333 <shb2in33@naver.com>
2024-02-09 13:26:26 +0900

    var & val

--- HelloWorld/src/main/kotlin/Main.kt
+++ HelloWorld/src/main/kotlin/Main.kt
@@ -1,3 +1,13 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+
+    // var : 변경 가능
+    // val : 변경 불가
+
+    var i: Int = 10
+    val j: Int = 10
+
+    i = 20
+//   j = 20
+    println(i)
+    println(j)
 }
