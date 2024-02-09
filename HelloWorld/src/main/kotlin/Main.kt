9368c7dd4f53af37413af9545d04244f875b5108
seunghyun333 <shb2in33@naver.com>
2024-02-09 13:34:04 +0900

    String ?

--- HelloWorld/src/main/kotlin/Main.kt
+++ HelloWorld/src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 fun main(args: Array<String>) {
 
+    // 1. var & val
     // var : 변경 가능
     // val : 변경 불가
 
@@ -11,7 +12,7 @@ fun main(args: Array<String>) {
     println(i)
     println(j)
 
-
+    // 2. ? Int와 String에 ? 붙였을 때 null 허용
 
     var k: Int = 10
     var l: Int? = 10
@@ -21,5 +22,16 @@ fun main(args: Array<String>) {
 
     println(k)
     println(l)
+
+    var s: String = "ABC"
+    var m: String? = "ABC"
+
+    //s = null
+    m = null
+
+    println(s)
+    println(m)
+
+
 
 }
